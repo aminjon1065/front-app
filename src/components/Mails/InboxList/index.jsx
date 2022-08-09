6dc2452f90b332cfac461c78c17b19404a9c177a
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2022-08-09 16:59:05 +0500

    0.0.1

--- src/components/Mails/InboxList/index.jsx
+++ src/components/Mails/InboxList/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {useGetMessagesQuery} from "../../../utils/getMessages";
 
 const Index = () => {
-
-
+    const {data = [], isLoading, error} = useGetMessagesQuery();
+    if (isLoading) return <h1>Loading...</h1>
     return (
         <>
+            <ul>
+                {
+                    data.map((msg)=>(
+                        <li key={msg.id}>
+                            {
+                                msg.text
+                            }
+                        </li>
+                    ))
+                }
+            </ul>
 
         </>
     );
