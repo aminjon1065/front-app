cd3a4e62da21a7e5aca958783505e1a3f44bc43e
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2022-08-25 14:56:10 +0500

    Revert "0.0.1"
    This reverts commit 53a53e83825a838de8ba3155fcc546014b9f90bd.

--- src/utils/queryMails.js
+++
@@ -1,7 +0,0 @@
-// import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react";
-// import {API_APP} from "../helper/CONSTANTS";
-// const token = localStorage.getItem("JWT_token")
-// export const queryMails =createApi({
-//     reducerPath:"mails",
-//     baseQuery:fetchBaseQuery({baseUrl:API_APP, prepareHeaders:{`Authorization`  ${token}`})
-// })
