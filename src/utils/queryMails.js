53a53e83825a838de8ba3155fcc546014b9f90bd
Aminjon Aliev <aminjon1065@gmail.com>
2022-08-25 14:42:56 +0500

    0.0.1

---
+++ src/utils/queryMails.js
@@ -0,0 +1,7 @@
+// import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react";
+// import {API_APP} from "../helper/CONSTANTS";
+// const token = localStorage.getItem("JWT_token")
+// export const queryMails =createApi({
+//     reducerPath:"mails",
+//     baseQuery:fetchBaseQuery({baseUrl:API_APP, prepareHeaders:{`Authorization`  ${token}`})
+// })
