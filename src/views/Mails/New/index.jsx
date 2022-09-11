756e16a0a7d04401c455085b491878a3b2d79afa
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2022-09-12 01:42:31 +0500

    0.0.1

--- src/views/Mails/New/index.jsx
+++ src/views/Mails/New/index.jsx
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
-import {Col, Container, FormLabel, Row} from "react-bootstrap";
+import {Col, Container, FormControl, FormLabel, Row} from "react-bootstrap";
 import Select from "react-select";
 import {useGetAllUsersQuery} from "../../../utils/getUsersList";
 
 const Index = () => {
     const [to, setTo] = useState([])
+    const [title, setTitle] = useState('')
     const {data = [], isLoading, error} = useGetAllUsersQuery();
-
+    const onChangeTitle = (e) => {
+        setTitle(e.target.value)
+    }
     if (error) return <h1 className="text-danger">Загрузка...</h1>
     return (
         <>
@@ -31,7 +34,35 @@ const Index = () => {
                                     placeholder={"Выберите кому отправить"}
                                 />
                         }
-
+                    </Col>
+                </Row>
+                <Row className="mt-1">
+                    <Col xs={3} sm={3} md={2} xl={2} xxl={2}>
+                        <FormLabel className="fs-6">
+                            Тема:
+                        </FormLabel>
+                    </Col>
+                    <Col xs={9} sm={9} md={10} xxl={10} xl={10}>
+                        <FormControl
+                            className="shadow-none"
+                            size={"sm"}
+                            value={title}
+                            onChange={onChangeTitle}
+                        />
+                    </Col>
+                </Row>
+                <Row className="mt-1">
+                    <Col xs={3} sm={3} md={2} xl={2} xxl={2}>
+                        <FormLabel className="fs-6">
+                            Текст:
+                        </FormLabel>
+                    </Col>
+                    <Col xs={9} sm={9} md={10} xxl={10} xl={10}>
+                        <FormControl as={"textarea"}
+                            className="shadow-none"
+                            value={title}
+                            onChange={onChangeTitle}
+                        />
                     </Col>
                 </Row>
             </Container>
