52e301300417e7bdb85ee5b65bac42d4dbb0ea28
Aminjon Aliev <aminjon1065@gmail.com>
2022-09-22 11:30:57 +0500

    0.0.1

--- src/components/Mails/InboxList/index.jsx
+++ src/components/Mails/InboxList/index.jsx
@@ -6,16 +6,25 @@ const Index = ({message, index}) => {
     return (
         <>
             <ListGroup.Item
-                className={`d-flex hover-list-item mb-1 ${message.opened ? 'bg-soft-grey' : 'fw-bold'}`}
+                className={`d-flex hover-list-item ${message.opened ? 'bg-soft-grey' : 'fw-bold'}`}
             >
                 <span className="text-slate pe-2">{index + 1}).</span>
                 {/*<Form.Check aria-label={message.id} className="px-1"/>*/}
-                <span className={`flex-fill`}>{message.title}</span>
+                <span
+                    className={`flex-fill`}
+                >
+                    {
+                        message.title
+                    }
+                </span>
                 <span
                     className="align-self-end fst-italic fz-1"
                 >
-                                {formatterDay(message.created_at)}
-                            </span>
+                                {
+                                    formatterDay(message.created_at)
+                                }
+
+                </span>
             </ListGroup.Item>
         </>
     )
